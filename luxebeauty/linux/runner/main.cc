105bf284306b5bf42daa6220241fc850b87b0527
GallegoMVP <gallegobranjuanjose@gmail.com>
2026-06-05 08:23:41 -0500

    fit/ se agrega el loginn

--- luxebeauty/linux/runner/main.cc
+++ luxebeauty/linux/runner/main.cc
@@ -2,6 +2,7 @@
 
 //Main
 int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = my_application_new();
+  g_autoptr(MyApplication) app = 
+  my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
